64ecbf6e3d33cb1673394d8f13457e7b5c409926
hamza-alkharouf <86008423+hamza-alkharouf@users.noreply.github.com>
2022-08-18 22:34:06 +0300

    add Singleton

--- src/designPatterns/singletonDesignPattern/Singleton.java
+++ src/designPatterns/singletonDesignPattern/Singleton.java
@@ -1,4 +1,32 @@
 package designPatterns.singletonDesignPattern;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Singleton {
+
+    private Integer[] numbers = {1,2,3,4,5,6,7};
+    private List<Integer> list = Arrays.asList(numbers);
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+           instance = new Singleton();
+        }
+        return instance;
+    }
+
+    private Singleton(){
+        System.out.println("Object Created");
+        Collections.shuffle(list);
+        System.out.println(this.hashCode());
+    }
+    public  void printer(){
+        for (Integer i:list){
+            System.out.print(i.toString()+" ");
+        }
+        System.out.println();
+    }
+
 }
--- src/designPatterns/singletonDesignPattern/main.java
+++ src/designPatterns/singletonDesignPattern/main.java
@@ -1,4 +1,15 @@
 package designPatterns.singletonDesignPattern;
 
 public class main {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        singleton.printer();
+        singleton2.printer();
+    }
+
+
+
 }
